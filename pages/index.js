ab5110fad384abf73c8dfc0325984274bb9cb242
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-08-10 03:11:52 +0000

    Frase modificada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Simplesmente fiz a pagina para dizer que:</h1>
+    return <h1>Dani simplesmente fiz a pagina para dizer que: tiamuuu, ok ok pode dar uma risadinha hehe</h1>
 }
 
 export default Home;
