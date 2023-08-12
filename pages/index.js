08d4b3811499f11c21e0ca040af856a8d6d8d8e2
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-08-12 03:59:30 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Dani simplesmente fiz a pagina para dizer que: tiamuuu, ok ok pode dar uma risadinha hehe</h1>
+  return (
+    <h1>
+      Dani simplesmente fiz a pagina para dizer que: tiamuuu, ok ok pode dar uma
+      risadinha hehe
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("teste");
+}
 export default Home;
