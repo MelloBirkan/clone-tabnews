f1bf7c2989fa990b513363e52acb293790640c30
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-08-15 23:03:37 +0000

    Foto removida devido instabilidades

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 function Home() {
   return (
-    <body>
-      <h1>
-        Dani simplesmente fiz a pagina para dizer que: TE AMO MUITO MAIS, ok ok
-        pode dar uma risadinha hehe
-      </h1>
-      <img src="https://cdn.discordapp.com/attachments/1012034545211670538/1141104441622265866/20230202_131921_Original.jpg"></img>
-    </body>
+    <h1>
+      Dani simplesmente fiz a pagina para dizer que: TE AMO MUITO MAIS, ok ok
+      pode dar uma risadinha hehe
+    </h1>
   );
 }
 
-function teste() {
-  console.log("teste");
-}
 export default Home;
