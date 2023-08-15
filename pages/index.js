3cae7a91264f218b5edc1f2b825bca5054c42094
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-08-15 20:23:57 +0000

    Arquivo pages editado

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home() {
   return (
-    <h1>
-      Dani simplesmente fiz a pagina para dizer que: TE AMO MUITO MAIS, ok ok
-      pode dar uma risadinha hehe
-    </h1>
+    <body>
+      <h1>
+        Dani simplesmente fiz a pagina para dizer que: TE AMO MUITO MAIS, ok ok
+        pode dar uma risadinha hehe
+      </h1>
+      <img src="https://cdn.discordapp.com/attachments/1012034545211670538/1141104441622265866/20230202_131921_Original.jpg"></img>
+    </body>
   );
 }
 
