8bc2abfa5fb8b11e1a7b5e3ad27c9a4f3e34ea34
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-09-16 17:51:57 -0300

    Mudanças no instalador

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 function Home() {
   return (
+    <div>
     <h1>
       Dani simplesmente fiz a pagina para dizer que: TIIII AMUUU MUITO MAIS, ok
       ok pode dar uma risadinha hehe
     </h1>
+    </div>
   );
 }
 
